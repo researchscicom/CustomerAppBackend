739514fa79b4ec54a8d8fded74da30b865c35ff7
haseeAmarathunga <haseeamarathunga@gmail.com>
2019-08-21 22:52:07 +0530

    Update Customer Web Service

--- src/main/java/com/customer/spring/controller/CustomerController.java
+++ src/main/java/com/customer/spring/controller/CustomerController.java
@@ -1,15 +1,23 @@
 package com.customer.spring.controller;
 
+import com.customer.spring.model.Customer;
+import com.customer.spring.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class CustomerController {
 
+    @Autowired
+    CustomerService customerService;
+
     @GetMapping("/customers")
-    public String getAllDetails()
+    public List<Customer> getAllDetails() throws NullPointerException
     {
-        return "Hey";
+        return customerService.getCustomers();
     }
 }
