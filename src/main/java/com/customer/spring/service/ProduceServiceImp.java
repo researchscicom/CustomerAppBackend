9125ec18863f0c262533d8910ba790788312b5a2
haseeAmarathunga <haseeamarathunga@gmail.com>
2019-08-26 11:26:47 +0530

    Update RabbitMQ Configuration

--- src/main/java/com/customer/spring/service/ProduceServiceImp.java
+++ src/main/java/com/customer/spring/service/ProduceServiceImp.java
@@ -1,12 +1,8 @@
 package com.customer.spring.service;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.*;
 import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
@@ -16,41 +12,36 @@ import java.util.concurrent.TimeoutException;
 @PropertySource("classpath:db.properties")
 @Service
 public class ProduceServiceImp implements ProducerService {
-    private static final String EXCHANGE_NAME = "messages";
+    private static final String EXCHANGE_NAME = "jsa.direct";
+    private static final String QUEUE_NAME = "jsa.queue";
 
-    private AmqpTemplate amqpTemplate;
-
-    @Value("jsa.rabbitmq.exchange")
-    private String exchange;
-
-    @Value("jsa.rabbitmq.routingkey")
-    private String routingkey;
+    private RabbitTemplate amqpTemplate;
 
     @Override
     public void produceData(String msg) {
-        try{
-            ConnectionFactory factory = new ConnectionFactory();
-            factory.setHost("localhost");
-            factory.setUsername("guest");
-            factory.setPassword("guest");
-            factory.setPort(15672);
-            Connection connection = factory.newConnection();
-            Channel channel = connection.createChannel();
-
-            channel.exchangeDeclare(EXCHANGE_NAME,"jpa.direct");
-            channel.basicPublish(EXCHANGE_NAME,"",null,msg.getBytes());
-            System.out.println("[x] sent '"+msg+"'");
-            channel.close();
-            connection.close();
-
-        }catch(IOException e){
-            System.out.println("IOException");
-            e.printStackTrace();
-        }catch(TimeoutException te){
-            System.out.println("TimeOut Exception");
-            te.printStackTrace();
-        }
-
-//        amqpTemplate.convertAndSend(exchange,routingkey,msg);
+//        try{
+//            ConnectionFactory factory = new ConnectionFactory();
+//            factory.setHost("localhost");
+//            factory.setVirtualHost("guest");
+//            factory.setUsername("guest");
+//            factory.setPassword("guest");
+//            factory.setPort(5672);
+//            Connection connection = factory.newConnection();
+//            Channel channel = connection.createChannel();
+//
+//            channel.exchangeDeclare(EXCHANGE_NAME,BuiltinExchangeType.DIRECT);
+//            channel.basicPublish(EXCHANGE_NAME,"jsa.routingkey",null,msg.getBytes());
+//            System.out.println("[x] sent '"+msg+"'");
+//            channel.close();
+//            connection.close();
+//
+//        }catch(IOException e){
+//            System.out.println("IOException");
+//            e.printStackTrace();
+//        }catch(TimeoutException te){
+//            System.out.println("TimeOut Exception");
+//            te.printStackTrace();
+//        }
+        //amqpTemplate.convertAndSend("jsa.direct","jsa.routingkey",msg);
     }
 }
