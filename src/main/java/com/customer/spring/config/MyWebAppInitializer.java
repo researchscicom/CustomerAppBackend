001251ffb06ba13444c2ca971916916f32c40073
haseeAmarathunga <haseeamarathunga@gmail.com>
2019-08-27 01:14:12 -0700

    Update Customer Model with Configuration

---
+++ src/main/java/com/customer/spring/config/MyWebAppInitializer.java
@@ -0,0 +1,20 @@
+package com.customer.spring.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { AppConfig.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] { com.customer.spring.config.WebConfig.class };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+}
---
+++ src/main/java/com/customer/spring/model/Customer.java
@@ -0,0 +1,78 @@
+package com.customer.spring.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
+    Long id;
+
+    @Column(name = "Name")
+    String name;
+
+    @Column(name = "Age")
+    String age;
+
+    @Column(name = "Email")
+    String email;
+
+    @Column(name = "City")
+    String city;
+
+    @Column(name = "Gender")
+    String gender;
+
+    public Customer() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
