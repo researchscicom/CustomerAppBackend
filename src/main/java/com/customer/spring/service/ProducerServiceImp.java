0b62f4fea24c53a504ab8673ce65e1411a354941
haseeAmarathunga <haseeamarathunga@gmail.com>
2019-09-02 09:46:57 +0530

    Update Properties

--- src/main/java/com/customer/spring/service/ProducerServiceImp.java
+++ src/main/java/com/customer/spring/service/ProducerServiceImp.java
@@ -1,6 +1,7 @@
 package com.customer.spring.service;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,9 +12,15 @@ public class ProducerServiceImp implements ProducerService {
     @Autowired
     private AmqpTemplate amqpTemplate;
 
+    @Value( "${rabbitmq.exchange}" )
+    private String exchange;
+
+    @Value( "${rabbitmq.routingkey}" )
+    private String routingkey;
+
     @Override
     public Object sendMsg(Long proId) throws Exception{
-        Object response = amqpTemplate.convertSendAndReceive("customer.direct","customer.routingkey",proId);
+        Object response = amqpTemplate.convertSendAndReceive(exchange,routingkey,proId);
         System.out.println("============== Response ==================");
         System.out.println(response);
         System.out.println("==========================================");
